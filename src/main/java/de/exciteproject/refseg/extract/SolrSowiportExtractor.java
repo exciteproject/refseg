06a3f713e7597e98a4af7197f568c04c7f778b3e
mkrnr <info@mkoerner.de>
2016-11-18 16:13:09 +0100

    Add first (test) version of SolrSowiportExtractor

---
+++ src/main/java/de/exciteproject/refseg/extract/SolrSowiportExtractor.java
@@ -0,0 +1,32 @@
+package de.exciteproject.refseg.extract;
+
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+
+public class SolrSowiportExtractor {
+
+    public static void main(String[] args) throws SolrServerException, IOException {
+        // URL is not publicly reachable
+        String urlString = "http://sowiportbeta.gesis.org/devwork/service/solr/solr_query.php";
+
+        SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+        SolrQuery query = new SolrQuery();
+        query.set("q", "person_author_txtP_mv:*");
+        query.set("fl", "id,person_author_txtP_mv");
+        query.set("rows", "100");
+        QueryResponse response = solr.query(query);
+        SolrDocumentList list = response.getResults();
+
+        System.out.println(list.size());
+        System.out.println("---");
+        System.out.println(list.get(0));
+
+    }
+
+}
