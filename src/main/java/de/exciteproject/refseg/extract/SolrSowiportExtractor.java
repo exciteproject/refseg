2551906f674c4ff6b84b92e2e8baa61d34f68dce
mkrnr <info@mkoerner.de>
2016-11-21 10:57:53 +0100

    Change solrj version number

--- src/main/java/de/exciteproject/refseg/extract/SolrSowiportExtractor.java
+++ src/main/java/de/exciteproject/refseg/extract/SolrSowiportExtractor.java
@@ -15,17 +15,20 @@ public class SolrSowiportExtractor {
         // URL is not publicly reachable
         String urlString = "http://sowiportbeta.gesis.org/devwork/service/solr/solr_query.php";
 
-        SolrClient solr = new HttpSolrClient.Builder(urlString).build();
+        SolrClient solr = new HttpSolrClient(urlString);
+
         SolrQuery query = new SolrQuery();
         query.set("q", "person_author_txtP_mv:*");
         query.set("fl", "id,person_author_txtP_mv");
         query.set("rows", "100");
+
         QueryResponse response = solr.query(query);
         SolrDocumentList list = response.getResults();
 
         System.out.println(list.size());
         System.out.println("---");
         System.out.println(list.get(0));
+        solr.close();
 
     }
 
